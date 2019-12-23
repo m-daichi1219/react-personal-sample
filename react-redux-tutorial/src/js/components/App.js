402dca7d74f9d4a8d98ebe034d0c99c8b24de2c2
m-daichi1219 <matu9525@gmail.com>
2019-12-23 22:27:37 +0900

    Add redux turorial

---
+++ react-redux-tutorial/src/js/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import List from './List';
+import Form from './Form';
+import Post from './Posts';
+
+const App = () => (
+  <>
+    <div>
+      <h2>Articles</h2>
+      <List />
+    </div>
+    <div>
+      <h2>Add a new Articles</h2>
+      <Form />
+    </div>
+    <div>
+      <h2>API Posts</h2>
+      <Post />
+    </div>
+  </>
+);
+
+export default App;
